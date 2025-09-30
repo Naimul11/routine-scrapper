d70251eca443024477af242304c7a0a01ef01620
naimul11 <naimulsanto5@gmail.com>
2025-09-30 12:08:17 +0600

    bug fix

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ CORS(app)  # allow calls from Flutter
 ERP_BASE = "http://sue.su.edu.bd:5081/sonargaon_erp"
 
 DAY_ORDER = {
-    'Sunday': 0,
-    'Monday': 1, 
-    'Tuesday': 2,
-    'Wednesday': 3,
-    'Thursday': 4,
-    'Friday': 5,
-    'Saturday': 6
+    'Saturday': 0,
+    'Sunday': 1,
+    'Monday': 2, 
+    'Tuesday': 3,
+    'Wednesday': 4,
+    'Thursday': 5,
+    'Friday': 6
 }
 
 # ------------------------
